93d28f71bcfbace118a1170a6850fdbcc7c57f5f
twiglau <twigliu@hotmail.com>
2025-09-11 21:03:08 +0800

    feat: aa

--- node-info/02-analyze/02-内存泄漏分析/MSVC/app.js
+++ node-info/02-analyze/02-内存泄漏分析/MSVC/app.js
@@ -5,6 +5,9 @@ const app = new Koa();
 
 const routerMiddleware = require('./src/middleware/router');
 const logCenter = require("./src/middleware/logCenter");
+const session = require('./src/middleware/session');
+
+const dumpFun = require('./src/lib/heapdump'); 
 
 // 在 app.js 或入口文件中
 app.use(async (ctx, next) => {
@@ -21,7 +24,13 @@ app.use(async (ctx, next) => {
 });
 
 app.use(logCenter());
+app.use(session());
 app.use(routerMiddleware());
 
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
+
+
+/// 为了方便，可以打开如下代码自动获取
+const currentDate = new Date();
+dumpFun('nodejs-cloumn', `${currentDate.getHours()}:${currentDate.getMinutes()+1}`, 60);
