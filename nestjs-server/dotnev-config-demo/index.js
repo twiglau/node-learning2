d35cb063a5040e69d5625ec90db38d1438118a15
twiglau <twigliu@hotmail.com>
2025-09-23 14:51:45 +0800

    feat: 修改内容

---
+++ nestjs-server/dotnev-config-demo/index.js
@@ -0,0 +1,15 @@
+// require('dotenv').config();
+
+// console.log(process.env);
+
+// const config = require("config");
+// const dbConfig = config.get("db");
+
+// console.log('~ file: index.js ~ line 7 ~ dbConfig', dbConfig);
+
+
+
+const config = require("config");
+const dbConfig = config.get("db");
+
+console.log('~ file: index.js ~ line 7 ~ dbConfig', dbConfig);
