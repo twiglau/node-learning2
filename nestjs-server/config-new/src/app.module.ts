ddadee2a4794ccd42eb59c532cd7fad1a25f933d
twiglau <twigliu@hotmail.com>
2025-09-25 20:54:00 +0800

    feat: 修改

--- nestjs-server/config-new/src/app.module.ts
+++ nestjs-server/config-new/src/app.module.ts
@@ -4,9 +4,13 @@ import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import Configuration from './configuration';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import Joi from 'joi';
 import { ConfigEnum } from './enum/config.enum';
+import { Logs } from './logs/logs.entity';
+import { Roles } from './roles/roles.entity';
+import { Profile } from './user/profile.entity';
+import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 
 // 1. 配置文件 方法一
@@ -28,26 +32,31 @@ const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
         NODE_ENV: Joi.string()
           .valid('development', 'production')
           .default('development'),
-        DB_PROT: Joi.number().default(3306),
-        DB_URL: Joi.string().domain(),
+        DB_PORT: Joi.number().default(3306),
         DB_HOST: Joi.string().ip(),
+        DB_TYPE: Joi.string().valid('mysql', 'postgres'),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
       }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
+      useFactory: (configService: ConfigService) => {
         return {
-          host: config.get(ConfigEnum.DB_HOST),
-          port: 3306,
-          username: 'root',
-          password: 'root',
-          database: 'testdb',
-          entities: [],
-          // 同步本地的 schema 与 数据库 -> 初始化的时候去使用
-          synchronize: true,
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.DB_HOST),
+          port: configService.get(ConfigEnum.DB_PORT),
+          username: configService.get(ConfigEnum.DB_USERNAME),
+          password: configService.get(ConfigEnum.DB_PASSWORD),
+          database: configService.get(ConfigEnum.DB_DATABASE),
+          entities: [User, Profile, Logs, Roles],
+          // 同步本地的schema与数据库 -> 初始化的时候去使用
+          synchronize: configService.get(ConfigEnum.DB_SYNC),
           logging: ['error'],
-        };
+        } as TypeOrmModuleOptions;
       },
     }),
     // TypeOrmModule.forRoot({
---
+++ nestjs-server/config-new/src/user/user.entity.ts
@@ -0,0 +1,30 @@
+import { Logs } from 'src/logs/logs.entity';
+import { Roles } from 'src/roles/roles.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  // typescript -> 数据库  关联关系  Mapping
+  @OneToMany(() => Logs, (logs) => logs.user)
+  logs: Logs[];
+
+  @ManyToMany(() => Roles, (roles) => roles.users)
+  @JoinTable({ name: 'users_roles' })
+  roles: Roles[];
+}
