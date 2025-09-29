558066079d4136149a42a4e4c691c58c84f1d37e
twiglau <twigliu@hotmail.com>
2025-09-29 20:49:35 +0800

    feat: logs模型添加

---
+++ nestjs-server/config-new/src/logs/logs.module.ts
@@ -0,0 +1,56 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { utilities, WinstonModule } from 'nest-winston';
+import { LogEnum } from 'src/enum/config.enum';
+import { LogsController } from './logs.controller';
+import { LogsService } from './logs.service';
+import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import * as Transports from 'winston/lib/winston/transports';
+
+function createDailyRotateTransport(level: string, filename: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
+@Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const consoleTransports = new Transports.Console({
+          level: 'info',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike(),
+          ),
+        });
+
+        return {
+          transports: [
+            consoleTransports,
+            ...(configService.get(LogEnum.LOG_ON)
+              ? [
+                  createDailyRotateTransport('info', 'application'),
+                  createDailyRotateTransport('warn', 'error'),
+                ]
+              : []),
+          ],
+        };
+      },
+    }),
+  ],
+  controllers: [LogsController],
+  providers: [LogsService],
+})
+export class LogsModule {}
