9b243096fe0b7d0fb613787903a909260c36b8e6
飞亚 <twigliu@hotmail.com>
2025-10-16 16:00:21 +0800

    feat: TypeORM 集成

--- nestjs-demo/src/app.controller.ts
+++ nestjs-demo/src/app.controller.ts
@@ -1,12 +1,29 @@
 // import { InjectRedis } from '@nestjs-modules/ioredis';
 import { MailerService } from '@nestjs-modules/mailer';
 import { Controller, Get, Version } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { Repository } from 'typeorm';
+// import { PrismaService } from './database/prisma/prisma.service';
 // import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
   //   constructor(@InjectRedis() private readonly redis: Redis) {}
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+    // private readonly prismaService: PrismaService,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  @Get('/prisma')
+  @Version('1')
+  async getHello() {
+    // const res = this.prismaService.user.findMany();
+    // return res;
+    const res = await this.userRepository.find();
+    return res;
+  }
 
   @Get()
   @Version('2')
--- nestjs-demo/src/app.module.ts
+++ nestjs-demo/src/app.module.ts
@@ -4,8 +4,32 @@ import { LogsModule } from './common/logger/logs.module';
 import { CacheModule } from './common/cache/cache.module';
 import { AppController } from './app.controller';
 import { MailModule } from './common/mail/mail.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from './user/user.entity';
+// import { PrismaModule } from './database/prisma/prisma.module';
 @Module({
-  imports: [ConfigModule, LogsModule, CacheModule, MailModule],
+  imports: [
+    ConfigModule,
+    LogsModule,
+    CacheModule,
+    MailModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PROT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: Boolean(configService.get('DB_AUTOLOAD')) || false,
+          synchronize: Boolean(configService.get('DB_SYNC')) || false,
+        }) as TypeOrmModuleOptions,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [],
 })
